aa6032f7d8a17d2e323a0ab8ea9231ca98f00c03
lenovo <ihozomerveille@gmail.com>
2026-05-03 12:32:25 +0200

    added a landing page

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,21 @@
-import MainLayout from '@/components/layout/MainLayout';
+import type { Metadata } from 'next';
+import '../globals.css';
 
-export default function PublicLayout({ children }) {
-  return <MainLayout>{children}</MainLayout>;
+export const metadata: Metadata = {
+  title: 'MedDelivery',
+  description: 'MedDelivery application',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
