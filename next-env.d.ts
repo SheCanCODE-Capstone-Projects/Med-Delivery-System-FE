4357a0a4bc54f42a41b404e82fdff5de9676fbde
Cynthy Niganze <cynthynigaira@gmail.com>
2026-05-10 20:48:54 +0200

    adding dependencies and solving some issues

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+<<<<<<< HEAD
 import "./.next/types/routes.d.ts";
 import "./.next/dev/types/routes.d.ts";
+=======
+/// <reference path="./.next/types/routes.d.ts" />
+>>>>>>> ccf2f60 (adding dependencies and solving some issues)
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
